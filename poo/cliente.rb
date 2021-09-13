e53435fdbb0bf120a4efb79a9511197e41a2ffbf
dabes1710 <david.belmar@mayor.cl>
2021-09-13 20:12:06 -0300

    poo

---
+++ poo/cliente.rb
@@ -0,0 +1,33 @@
+class Cliente
+    # atributos /variables /datos
+
+    #metodos /Acciones /funciones
+
+    def saludar
+        puts "Hola soy un cliemte"
+    end
+    
+    def imprimir_datos
+    end
+    
+    def comprar_frutas
+    end
+    
+    def actualizar_datos
+    end
+
+end
+
+cliente1 = cliente.new
+cliente1.saludar
+
+
+cliente2 = cliente.new
+cliente2.saludar
+
+cliente3 = cliente.new
+cliente3.saludar
+
+cliente4 = cliente.new
+cliente4.saludar
+
